17d1258ed0a29190ab91f00d4e00043fccb55c30
grytsaidmitry <dimasicus2008@gmail.com>
2026-03-13 21:51:48 +0100

    Add files via upload

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import './js/pets-list.js';
-import './js/FAQ.js';
-import { render } from './js/render-stories';
-render();
-import './js/modal-function.js';
-import './about_us.js';
-import './js/cat_decor.js';
-import './js/header.js';
